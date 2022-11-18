a15e0cfbb5f38d898396fd21d915a40527ed2b0e
Vinicius José <viniciusjosedev@gmail.com>
2022-11-18 11:40:30 -0300

    requisito 4 finalizado

--- src/main.js
+++ src/main.js
@@ -3,14 +3,26 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-async function teste() {
-  const pull = await fetchProductsList('computador gamer');
+function loading(param) {
+  if (param === true) {
+    const tagP = document.createElement('p');
+    tagP.className = 'loading';
+    tagP.innerText = 'carregando...';
+    return document.getElementsByClassName('products')[0]
+      .appendChild(tagP);
+  } return document.getElementsByClassName('loading')[0].remove();
+}
+
+async function search() {
+  loading(true);
+  const pull = await fetchProductsList('rx 580 4gb');
+  loading(false);
   pull.forEach((elemento) => {
     document.getElementsByClassName('products')[0]
       .appendChild(createProductElement(elemento));
   });
 }
 
-teste();
+search();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
